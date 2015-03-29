c18893e12f46e32c9a27d0e4f438594f44bb3d84
Austin Liu <berkamin@gmail.com>
2015-03-28 23:47:14 -0700

    Implemented queue with linked list structure.
    I managed to implement a queue with a linked list structure; I did not
    need to implement linked lists separately.

--- Recursive-BFS.js
+++ Recursive-BFS.js
@@ -1,7 +1,34 @@
-// Linked list implementation
-// Queue will be built using this linked list
+// Queue based on linked list. Items are enqueued into the tail, and dequeued from the head.
 
-// Queue implementation
+var Queue = function(){ // functional class
+  var q = {}
+  q.head = null;
+  q.tail = null;
+
+  var Node = function(value){ // functional class
+    var n = {};
+    n.value = value;
+    n.next = null;
+    return n;
+  };
+
+  q.enqueue = function(value){
+    var newNode = Node(value);
+    if (this.tail !== null) { this.tail.next = newNode }
+    if (this.head === null) { this.head = newNode }
+    this.tail = newNode;
+  };
+
+  q.dequeue = function(){
+    if (this.head === null) { return null }
+    var result = this.head.value;
+    this.head = this.head.next;
+    return result;
+  };
+
+  return q;
+};
 
 // Binary Search Tree implementation
 // BFS queue will use the Queue object implemented above.
+
